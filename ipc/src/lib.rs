040dc6cfb9fc4f9fd796c7e3dfd1052d00dcf8f1
amitniz <amitnzri@gmail.com>
2023-01-07 10:59:06 +0200

    update: prototype implementation to the system's IPC struct.

--- ipc/src/lib.rs
+++ ipc/src/lib.rs
@@ -1,70 +1,121 @@
-pub struct MessagesBox{
-    jobs:Vec<Job>,
-    responds:Vec<Respond>,
-    pending:bool
+#[macro_use]
+extern crate lazy_static;
+use std::sync::Mutex;
+
+lazy_static! {
+    static ref GLOBAL_INSTANCE: Mutex<MessagesBox> = Mutex::new(MessagesBox::init());
 }
 
-impl MessagesBox{
-   
-	pub fn init() -> Self{
-		MessagesBox { jobs: vec![], responds: vec![], pending: false }
-	}
+/// A singleton that is used for IPC communication between the UI thread and the SherlockManager's thread.
+pub struct MessagesBox {
+    jobs: Vec<Job>,
+    responds: Vec<Respond>,
+    pending: bool,
+}
 
-	pub fn send_jobs(&mut self, jobs:Vec<Job>){
-		self.jobs.extend(jobs);
-		self.pending = true;
+impl MessagesBox {
+    fn init() -> Self {
+        MessagesBox {
+            jobs: vec![],
+            responds: vec![],
+            pending: false,
+        }
     }
 
-     pub fn recieve_jobs(&mut self) -> Vec<Job>{
+    fn get_instance() -> &'static GLOBAL_INSTANCE {
+        &GLOBAL_INSTANCE
+    }
+
+    pub fn send_jobs(&mut self, jobs: Vec<Job>) {
+        self.jobs.extend(jobs);
+        self.pending = true;
+    }
+
+    pub fn recieve_jobs(&mut self) -> Vec<Job> {
         let out = self.jobs.clone();
-		self.jobs.clear();
-		out
-     }
+        self.jobs.clear();
+        out
+    }
 
-	 pub fn send_responds(&mut self,responds:Vec<Respond>){
-		self.responds.extend(responds);
-	 }
+    pub fn send_responds(responds: Vec<Respond>) {
+        let m = MessagesBox::get_instance();
+        m.lock().unwrap().responds.extend(responds);
+        m.lock().unwrap().pending = true;
+    }
 
-	 pub fn finish(&mut self){
-		self.pending = false;
-	 }
+    pub fn finish() {
+        let m = MessagesBox::get_instance();
+        m.lock().unwrap().pending = false;
+    }
 
-     pub fn recieve_responds() -> Vec<Respond>{
-        todo!()
-     }
+    pub fn is_pending() -> bool {
+        let m = MessagesBox::get_instance();
+        m.lock().unwrap().pending
+    }
+
+    pub fn recieve_responds() -> Vec<Respond> {
+        let m = MessagesBox::get_instance();
+        let out = m.lock().unwrap().responds.clone();
+        m.lock().unwrap().responds.clear();
+        out
+    }
 }
 
-
-#[derive(Clone)]
-pub enum Job{
-	ListEngines,
-	RunEninges{engines_list:Vec<String>,query:String},
+#[derive(Clone, Debug)]
+pub enum Job {
+    ListEngines,
+    RunEninges {
+        engines_list: Vec<String>,
+        query: String,
+    },
 }
 
-
-pub enum Respond{
-	EngineResult{engine:String,output:String},
-	Message(String),
-	Error(String),
+#[derive(Clone, Debug)]
+pub enum Respond {
+    EngineResult { engine: String, output: String },
+    Message(String),
+    Error(String),
 }
 
 #[cfg(test)]
-mod messages_box_tests{
+mod messages_box_tests {
     use super::*;
+    use std::thread;
 
-	#[test]
-	fn clear_queues_after_read(){
-		// init
-		let mut ipc = MessagesBox::init();
-		// send a job
-		ipc.send_jobs(vec![Job::ListEngines]);
-		assert_eq!(ipc.jobs.len(),1);
-		// recieve a job
-		let jobs = ipc.recieve_jobs();
-		// make sure that the instance's jobs queue is clear
-		// and that the recievied jobs vector is not.
-		assert_eq!(ipc.jobs.len(),0);
-		assert_ne!(jobs.len(),ipc.jobs.len());
-	}
+    #[test]
+    fn clear_queues_after_read() {
+        // init
+        let mut ipc = MessagesBox::init();
+        // send a job
+        ipc.send_jobs(vec![Job::ListEngines]);
+        assert_eq!(ipc.jobs.len(), 1);
+        // recieve a job
+        let jobs = ipc.recieve_jobs();
+        // make sure that the instance's jobs queue is clear
+        // and that the recievied jobs vector is not.
+        assert_eq!(ipc.jobs.len(), 0);
+        assert_ne!(jobs.len(), ipc.jobs.len());
+    }
 
+    #[test]
+    fn multithread_test() {
+        thread::spawn(move || {
+            for i in 1..10 {
+                let msg = Respond::Message(i.to_string());
+                let responds = vec![msg];
+                println!("sent{:?}", responds);
+                MessagesBox::send_responds(responds);
+            }
+            MessagesBox::finish();
+        });
+
+        let handle = thread::spawn(move || {
+            while MessagesBox::is_pending() {
+                let res = MessagesBox::recieve_responds();
+                println!("recieved {:?}", res);
+            }
+        });
+
+        handle.join().unwrap();
+    }
 }
